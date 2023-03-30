174d0214df809ab037b3a60aae6509aa84a832fb
Ahmed Ali <alii403304@gmail.com>
2023-03-30 14:07:59 +0200

    configured cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const connect = require('./db/connect')
 const userRouter = require('./routes/user')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(cors({
+    origin: '*'
+}));
 app.use('/user', userRouter)
 connect()
 
